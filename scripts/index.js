a0a9bded86b26b16389fbeabd8e9a4a30a270a0d
Alfredo Hurtado <alfhur@yahoo.com>
2026-04-17 18:32:00 -0600

    fix(default-cards): S08-E3-Final-v2 Remediación observaciones entrega 1: se cargan tarjetas desde array

--- scripts/index.js
+++ scripts/index.js
@@ -38,6 +38,33 @@ const imageCloseButton = imageSection.querySelector(".popup__close");
 const imageName = imageSection.querySelector(".popup__caption");
 const imageElement = imageSection.querySelector(".popup__image");
 
+let initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
 function openModal(form) {
   console.log("openModal().Abriendo modal.");
   form.classList.add("popup_is-opened");
@@ -153,6 +180,11 @@ function renderCard(name, link, cardContainer) {
   //cardContainer.append(newCard);
   cardContainer.prepend(newCard);
 }
+
+initialCards.forEach(function (card) {
+  console.log("Card name: " + card.name + ", Card link: " + card.link);
+  renderCard(card.name, card.link, cardsContainer);
+});
 
 /**************************************************************************
  * Funciones para la creación de tarjetas a través del popup de creación de tarjeta
